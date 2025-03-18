ccf0615b7e3cd4afb5cbeb3ec4373b29b55a6762
Malaika Ashraf BCS22015 <saadsheilh420@gmail.com>
2025-03-19 01:32:20 +0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,110 @@
+#1. String Print:
+print("Hello world")
+
+#2.Comments
+x=1
+# The initial value of x is 1
+if x>0:
+    print("These are two comments") # print a string
+
+#3. User Input
+txt=input("type something to test it:")
+
+#4. Print Multiple statements on a single line:
+
+print ("Malaika")
+print ("Ashraf")
+
+
+# also print them in a single line as :
+print ("Malaika" , "Ashraf")
+
+#5 Indentation
+
+x=1
+if x>0:
+ print("Single space indentation") # print a string
+
+#6. Single space + tab indentation
+
+x=1
+if x>0:
+    print("Single space + tab indentation") # print a string
+
+#7. Identify number by using type() function:
+
+a = 123
+print(type(a))  # <class 'int'>
+b = 2.12e-10
+print(type(b))  # <class 'float'>
+
+
+c = 5E220
+print(type(c))  # <class 'float'>
+
+
+d = -123
+print(type(d))  # <class 'int'>
+
+#8. Complex Numbers
+
+a=complex(1,2)
+print(type(a))
+print(a)
+b= 1+2j
+print(type(b))
+
+#9. Bool Datatype
+
+x=True
+print(type(x))
+y=False
+print(type(y))
+
+# 10.String Print
+a="Malaika Ashraf"
+print(a)
+
+#11. string slicing
+
+# single character statements
+s="python tutorial"
+print(s[-11])
+print(s[11])
+print(s[-14])
+
+# 12 List Indices
+
+# empty list
+m=[]
+print(m)
+
+# 13. List
+m=["red","blue","green"]
+print(m[-1])
+print(m[2])
+print(m[4])
+
+ # 14. List Slicing
+
+m=["red","blue","green"]
+print(m[:2])
+print(m[:])
+print(m[1:2])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
